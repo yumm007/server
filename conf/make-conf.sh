2a6e8f0e667314c05d780b398353d3f5b16cdcab
Himm <yumm007@gmail.com>
2012-08-08 12:54:47 +0800

    sync code

--- conf/make-conf.sh
+++ conf/make-conf.sh
@@ -15,21 +15,21 @@ echo "USER_PWD = "`expr $IP + 11000 + 1`>> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "USER_NAME = default" >> $FILE_NAME
 echo "" >> $FILE_NAME
-echo "TRACKER_SERVER = 192.168.66.211" >> $FILE_NAME
+echo "TRACKER_SERVER = 192.168.160.211" >> $FILE_NAME
 echo "TRACKER_PORT = 5080" >> $FILE_NAME
-echo "PROXY_SERVER = 192.168.66.211" >> $FILE_NAME
+echo "PROXY_SERVER = 192.168.160.211" >> $FILE_NAME
 echo "PROXY_PORT = 5080" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "DEF_NW_SSID = SafeToneT01" >> $FILE_NAME
-echo "DEF_NW_CH = 1" >> $FILE_NAME
+echo "DEF_NW_CH = 11" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "SCAN_NW_SSID = SafeToneT01" >> $FILE_NAME
-echo "SCAN_NW_CH = 1" >> $FILE_NAME
+echo "SCAN_NW_CH = 11" >> $FILE_NAME
 echo "" >> $FILE_NAME
-echo "STATIC_IP_FLAG = 1" >> $FILE_NAME
-echo "DEF_IP_ADDR = 192.168.66."`expr $IP + 1` >> $FILE_NAME
+echo "STATIC_IP_FLAG = 0" >> $FILE_NAME
+echo "DEF_IP_ADDR = 192.168.160."`expr $IP + 1` >> $FILE_NAME
 echo "DEF_NET_MASK = 255.255.255.0" >> $FILE_NAME
-echo "DEF_NET_SW = 192.168.66.254" >> $FILE_NAME
+echo "DEF_NET_SW = 192.168.160.1" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "SENSER_TIMER = 10" >> $FILE_NAME
 echo "" >> $FILE_NAME
